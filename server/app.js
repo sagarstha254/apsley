2ae54c50a8069c3be4259858b81f268bb0f0af52
sagarstha254 <sagarstha254@gmail.com>
2023-08-29 19:41:10 +0545

    cors added from env

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(cors(
   {
-    origin:["https://apsley-beta.vercel.app"],
+    origin:process.env.CORS_LIST,
     methods:["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true
   }
