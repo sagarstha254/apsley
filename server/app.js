f5ce8b39d750ba02d252114b946399afecc62a1c
sagarstha254 <sagarstha254@gmail.com>
2023-08-29 21:15:28 +0545

    app updated

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ app.use(express.json());
 //CORS Headers
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", '*');
+  res.setHeader("Access-Control-Allow-Origin: https://apsley-beta.vercel.app", '*');
   res.setHeader(
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
