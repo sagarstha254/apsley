4edd504c6b46d51c16bf6d739c78b2fcbf173fac
sagarstha254 <sagarstha254@gmail.com>
2023-08-29 20:08:32 +0545

    mrf

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ app.use(express.json());
 //CORS Headers
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", process.env.CORS_LIST);
+  res.setHeader("Access-Control-Allow-Origin", '*');
   res.setHeader(
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
